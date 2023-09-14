04c522e8a6f531c87abaa05e8e5a9f5f647275da
SorraimiRivas <sorraimi.rivas@hotmail.com>
2023-09-14 04:19:07 -0400

    :tada: setup project structure, layout, background, typography(temp) and navigation/ header

---
+++ data.ts
@@ -0,0 +1,51 @@
+export const navigation = [
+  {
+    name: "Home",
+    path: "/#home",
+  },
+  {
+    name: "About",
+    path: "/#about",
+  },
+  {
+    name: "Projects",
+    path: "/#projects",
+  },
+  {
+    name: "Skills",
+    path: "/#skills",
+  },
+  {
+    name: "Experience",
+    path: "/#experience",
+  },
+  {
+    name: "Contact",
+    path: "/#contact",
+  },
+] as const;
+
+export const socials = [
+  {
+    name: "Github",
+    link: "",
+    icon: "github",
+  },
+  {
+    name: "LinkedIn",
+    link: "",
+    icon: "linkedin",
+  },
+] as const;
+
+export const skills = [
+  { name: "HTML", logo: "html5" },
+  { name: "CSS", logo: "css3" },
+  { name: "JavaScript", logo: "javascript" },
+  { name: "TypeScript", logo: "typescript" },
+  { name: "React", logo: "react" },
+  { name: "React Native", logo: "react" },
+  { name: "Expo", logo: "expo" },
+  { name: "MongoDB", logo: "mongodb" },
+  { name: "Axios HTTP", logo: "" },
+] as const;
