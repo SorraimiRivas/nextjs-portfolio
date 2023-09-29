67e00828eaf1a8ea59b0db04f10a5baac67b64c2
Sorraimi Rivas <sorraimi.rivas@hotmail.com>
2023-09-29 05:01:18 -0400

    :dizzy: added animation to missing elements to keep consistency across the page

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import React, { Fragment } from "react";
+import { motion } from "framer-motion";
 
 import SectionHeading from "./common/SectionHeading";
 import { projects } from "@/lib/data";
@@ -11,7 +12,14 @@ const Projects = () => {
   const { ref } = useSelectedSection("Projects", 0.2);
 
   return (
-    <section id="projects" className="scroll-mt-28" ref={ref}>
+    <motion.section
+      id="projects"
+      className="scroll-mt-28"
+      ref={ref}
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      transition={{ delay: 0.3, duration: 1 }}
+    >
       <SectionHeading>Projects</SectionHeading>
       <div>
         {projects.map((project) => (
@@ -20,7 +28,7 @@ const Projects = () => {
           </Fragment>
         ))}
       </div>
-    </section>
+    </motion.section>
   );
 };
 
--- components/common/SectionHeading.tsx
+++ components/common/SectionHeading.tsx
@@ -1,7 +1,17 @@
 import React from "react";
+import { motion } from "framer-motion";
 
 const SectionHeading = ({ children }: { children: React.ReactNode }) => {
-  return <h2 className="text-4xl font-medium py-10 text-center">{children}</h2>;
+  return (
+    <motion.h2
+      className="text-4xl font-medium py-10 text-center"
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      transition={{ delay: 0.3 }}
+    >
+      {children}
+    </motion.h2>
+  );
 };
 
 export default SectionHeading;
